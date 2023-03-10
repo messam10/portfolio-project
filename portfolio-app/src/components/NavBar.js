204bce1aa4ddbd796d01dd8f12451f12c05df51c
messam10 <memaq1998.10.7@gmail.com>
2023-03-10 10:06:43 +0300

    Add Banner Components

--- portfolio-app/src/components/NavBar.js
+++ portfolio-app/src/components/NavBar.js
@@ -47,7 +47,7 @@ function NavBar() {
               <a href="#"><img src={navIcon2} alt="" /></a>
               <a href="#"><img src={navIcon3} alt="" /></a>
             </div>
-            <button className="vvd" onClick={() => { console.log("connect") }}><span>Let"s Connect</span></button>
+            <button className="vvd" onClick={() => { console.log("connect") }}><span>Let's Connect</span></button>
           </span>
         </Navbar.Collapse>
       </Container>
